4e2f9d59d3093941b6c78d56e8a56ae17e77b90d
kanana <chalithbandara99@gmail.com>
2025-05-19 17:58:42 +0530

    Add sample comment for testing in User model

--- src/models/User.js
+++ src/models/User.js
@@ -24,4 +24,5 @@ UserSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
+//Sample comment fot the testing
 module.exports = mongoose.model('User', UserSchema);
